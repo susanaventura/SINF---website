54d6a7a94b1b81978fa1366bdc960ca3325c6435
LuisEduardoReis <luiseduardo.mreis@gmail.com>
2015-11-14 16:07:23 +0000

    Added exception handling to http requests

--- app/helpers/primavera_integration_helper.rb
+++ app/helpers/primavera_integration_helper.rb
@@ -2,28 +2,36 @@ module PrimaveraIntegrationHelper
   require 'net/http'
 
   def get_categories
-	JSON.parse send_get('categories').body
-	
+	  if (res = send_get('categories'))
+      JSON.parse res.body
+    else
+      {}
+    end
   end
 
 
-  
   private
   
-  def primavera_path(url)
-	'http://localhost:49275/api/'+url
-  end
-  
-  def send_get(resource)
-  
-	url = URI.parse(primavera_path(resource))
-	req = Net::HTTP::Get.new(url.to_s)
-	
-	puts 'Sending GET request to ' + url.to_s
-	
-	Net::HTTP.start(url.host, url.port) {|http|
-		http.request(req)
-	}
-  end
+    def primavera_path(url)
+      'http://localhost:49275/api/'+url
+    end
+
+    def send_get(resource)
+
+      url = URI.parse(primavera_path(resource))
+      req = Net::HTTP::Get.new(url.to_s)
+
+      puts 'Sending GET request to ' + url.to_s
+
+      begin
+        response = Net::HTTP.start(url.host, url.port) {|http|
+          http.request(req)
+        }
+      rescue Errno::ECONNREFUSED, Timeout::Error, Errno::EINVAL, Errno::ECONNRESET, EOFError, Net::HTTPBadResponse, Net::HTTPHeaderSyntaxError, Net::ProtocolError
+        response = nil
+      end
+
+      response
+    end
   
 end
