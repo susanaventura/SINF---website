fcaa2d5b00a12a79263cf12d48de03cd974cd112
susanaventura <ei12009@fe.up.pt>
2015-11-15 19:32:58 +0000

    finish products list layout

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,9 +1,31 @@
+ready = function() {
+    $('.dropdown-menu:hover').parent().css('background:#B81D22');
 
-$('.dropdown-menu:hover').parent().css('background:#B81D22');
+    $(".dropdown-menu").hover(
+        function () {
+            $(this).parent('.dropdown').css('background:#B81D22');
+        }, function () {
+        }
+    );
 
-$( ".dropdown-menu" ).hover(
-    function() {
-        $( this ).parent('.dropdown').css('background:#B81D22');
-    }, function() {
-    }
-);
+    var maxProductsHeight = Math.max.apply(null, $("div.product-desc").map(function ()
+    {
+        return $(this).outerHeight();
+    }).get());
+
+    $('div.product-desc').outerHeight(maxProductsHeight);
+
+/*
+    $('div.product').hover(function(){
+       $(this).height($(this).height()+5);
+        $(this).width($(this).width()+5);
+        $(this).css('margin-bottom:0px');
+    }, function(){
+        $(this).height($(this).height()-5);
+        $(this).width($(this).width()-5);
+    });
+*/
+};
+
+$(document).ready(ready);
+$(document).on('page:load', ready);
